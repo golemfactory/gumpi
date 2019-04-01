bca45e415b5305a20964645aa6f6de4991396458
Marcin Mielniczuk <marmistrz.dev@zoho.eu>
2019-04-01 20:19:48 +0200

    Make the script more robust

--- docker/run-tests.sh
+++ docker/run-tests.sh
@@ -1,12 +1,29 @@
+#!/bin/sh
+
+check_cmd() {
+	if ! command -v "$1" > /dev/null
+	then
+		echo "Error: program missing from PATH: $1. Exiting..."
+		exit 1
+	fi
+}
+
+cleanup() {
+	docker-compose down
+}
+trap cleanup EXIT
+
+check_cmd jq
+
 set -x
-docker-compose up -d 
+set -e
+docker-compose up -d
 
 HUB_ADDR=$(docker-compose exec hub gu-hub --json lan list -I hub | grep -v INFO | jq -r '.[0].Addresses')
 
-
 for idx in $(seq 1 4)
-do 
-	docker-compose exec --index=$idx prov gu-provider hubs connect "$HUB_ADDR"
+do
+	docker-compose exec --index="$idx" prov gu-provider hubs connect "$HUB_ADDR"
 done
 
 sleep 2
@@ -14,6 +31,3 @@ sleep 2
 docker-compose exec hub gu-hub peer list
 
 docker-compose exec hub gumpi -h "$HUB_ADDR" -j /examples/game-life.toml -n 4
-
-docker-compose down
-
