86b657672ca906245b10ca062a6439ca82a5a5d2
Marcin Mielniczuk <marmistrz.dev@zoho.eu>
2019-04-14 12:14:51 +0200

    WIP rework deploy

--- src/session/mpi.rs
+++ src/session/mpi.rs
@@ -4,22 +4,20 @@ use crate::{
     jobconfig::{BuildType, Sources},
     session::gu_client_ext::PeerHardwareQuery,
 };
-use failure::{Fail, Fallible, ResultExt};
+use failure::{Fallible, ResultExt};
 use futures::{
     future::{self, Either},
     prelude::*,
-    stream,
 };
-use gu_client::r#async::{HubConnection, HubSession, HubSessionRef, Peer, PeerSession};
+use gu_client::r#async::{HubConnection, HubSession, PeerSession};
 use gu_hardware::actor::Hardware;
 use gu_model::{
-    envman::{Command, CreateSession, DestroySession, Image, ResourceFormat},
+    envman::{Command, CreateSession, Image},
     peers::PeerInfo,
     session::HubSessionSpec,
 };
-
-use log::{info, warn};
-use std::net::SocketAddr;
+use log::info;
+use std::{net::SocketAddr, path::Path};
 
 #[derive(Debug)]
 pub struct ProviderMPI {
@@ -210,9 +208,9 @@ impl SessionMPI {
 
         /*info!("Downloading the hostfile...");
         let download_output = root.download(blob_id, "hostfile".to_owned(), ResourceFormat::Raw);
-        info!("Downloaded: {:?}", download_output);
+        info!("Downloaded: {:?}", download_output);*/
 
-        info!("Executing mpirun with args {:?}...", cmdline);*/
+        info!("Executing mpirun with args {:?}...", cmdline);
         let exec_cmd = Command::Exec {
             executable: "mpirun".to_owned(),
             args: cmdline,
@@ -221,79 +219,71 @@ impl SessionMPI {
             .update(vec![exec_cmd])
             .and_then(|mut outs| Ok(outs.swap_remove(0)))
             .from_err()
-
-        /*let ret = root.exec_commands(vec![exec_cmd])?;
-        println!("Output:");
-        for out in ret {
-            println!("{}\n========================", out);
-        }
-        Ok(())*/
     }
 
-    /*
-        // Returns: the deployment prefix
-        pub fn deploy(
-            &self,
-            config_path: &Path,
-            sources: &Sources,
-            progname: &str,
-        ) -> Fallible<String> {
-            let app_path = "app".to_owned();
-            let tarball_path = config_path.join(&sources.path);
+    // FIXME lifetimes are broken
+    // Returns: the deployment prefix
+    pub fn deploy(
+        &self,
+        config_path: &Path,
+        sources: &Sources,
+        progname: &str,
+    ) -> impl Future<Item = Vec<Vec<String>>, Error = failure::Error> {
+        let app_path = "app".to_owned();
+        let tarball_path = config_path.join(&sources.path);
 
-            let blob_id = self
-                .hub_session
-                .upload_file(&tarball_path)
-                .context("uploading file")?;
+        /*let blob_id = self
+        .hub_session
+        .upload_file(&tarball_path)
+        .context("uploading file")?;*/
 
-            for provider in &self.provider_sessions {
-                provider
-                    .download(blob_id, app_path.clone(), ResourceFormat::Tar)
-                    .context("downloading file")?;
+        let build_futs = self.providers.iter().map(|provider| {
+            /*provider
+            .download(blob_id, app_path.clone(), ResourceFormat::Tar)
+            .context("downloading file")?;*/
 
-                // If we create a ProviderSession per provider, every session
-                // gets a unique identifier. This means that the resulting executable
-                // resides in a different directory on each of the provider nodes,
-                // which causes mpirun to fail.
-                // As a workaround, we provide a symlink to the /tmp directory
-                // in the image and put the resulting binary there.
+            // If we create a ProviderSession per provider, every session
+            // gets a unique identifier. This means that the resulting executable
+            // resides in a different directory on each of the provider nodes,
+            // which causes mpirun to fail.
+            // As a workaround, we provide a symlink to the /tmp directory
+            // in the image and put the resulting binary there.
 
-                // For the CMake backend we use the EXECUTABLE_OUTPUT_PATH CMake variable
-                // For the Make backend we just move the file around
+            // For the CMake backend we use the EXECUTABLE_OUTPUT_PATH CMake variable
+            // For the Make backend we just move the file around
 
-                let cmake_cmd = Command::Exec {
-                    executable: "cmake/bin/cmake".to_owned(),
-                    args: vec![
-                        app_path.clone(),
-                        "-DCMAKE_C_COMPILER=mpicc".to_owned(),
-                        "-DCMAKE_CXX_COMPILER=mpicxx".to_owned(),
-                        "-DCMAKE_BUILD_TYPE=Release".to_owned(),
-                        "-DEXECUTABLE_OUTPUT_PATH=tmp".to_owned(), // TODO fix path for Make
-                    ],
-                };
-                let mv_cmd = Command::Exec {
-                    executable: "mv".to_owned(),
-                    args: vec![[&app_path, progname].join("/"), "tmp/".to_owned()],
-                };
-                let make_cmd = Command::Exec {
-                    executable: "make".to_owned(),
-                    args: vec![],
-                };
+            let cmake_cmd = Command::Exec {
+                executable: "cmake/bin/cmake".to_owned(),
+                args: vec![
+                    app_path.clone(),
+                    "-DCMAKE_C_COMPILER=mpicc".to_owned(),
+                    "-DCMAKE_CXX_COMPILER=mpicxx".to_owned(),
+                    "-DCMAKE_BUILD_TYPE=Release".to_owned(),
+                    "-DEXECUTABLE_OUTPUT_PATH=tmp".to_owned(), // TODO fix path for Make
+                ],
+            };
+            let mv_cmd = Command::Exec {
+                executable: "mv".to_owned(),
+                args: vec![[&app_path, progname].join("/"), "tmp/".to_owned()],
+            };
+            let make_cmd = Command::Exec {
+                executable: "make".to_owned(),
+                args: vec![],
+            };
 
-                let cmds = match &sources.mode {
-                    BuildType::Make => vec![make_cmd, mv_cmd],
-                    BuildType::CMake => vec![cmake_cmd, make_cmd],
-                };
+            let cmds = match &sources.mode {
+                BuildType::Make => vec![make_cmd, mv_cmd],
+                BuildType::CMake => vec![cmake_cmd, make_cmd],
+            };
 
-                let out = provider
-                    .exec_commands(cmds)
-                    .context(format!("compiling the app on node {}", provider.name()))?;
-                for out in out {
-                    info!("Provider {} compilation output:\n{}", provider.name(), out);
-                }
-            }
-            Ok("/tmp/".to_owned())
-        }
+            provider
+                .session
+                .update(cmds)
+                .context(format!("compiling the app on node {:?}", provider))
+        });
+        let build = future::join_all(build_futs);
+        // FIXME return build instead of a dummy thing
+        future::ok(vec![vec![]])
+        //Ok("/tmp/".to_owned())
     }
-    */
 }
