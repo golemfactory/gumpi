16133ac54aff72eeec701be03cc74f7d01ad33e5
Marcin Mielniczuk <marmistrz.dev@zoho.eu>
2019-04-18 08:59:06 +0200

    Automatically check correctness.

--- docker/run-tests.sh
+++ docker/run-tests.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 check_cmd() {
-	if ! command -v "$1" > /dev/null
-	then
+	if ! command -v "$1" >/dev/null; then
 		echo "Error: program missing from PATH: $1. Exiting..."
 		exit 1
 	fi
@@ -14,21 +13,22 @@ cleanup() {
 }
 trap cleanup EXIT
 
-
 set -x
 set -e
 docker-compose up -d
 
 HUB_ADDR=$(docker-compose exec hub gu-hub --json lan list -I hub | grep -v INFO | jq -r '.[0].Addresses')
 
-for idx in $(seq 1 4)
-do
+for idx in $(seq 1 4); do
 	docker-compose exec --index="$idx" prov gu-provider hubs connect "$HUB_ADDR"
 done
 
+# The providers need a while to connect to the hub, give them that while.
 sleep 2
 
 docker-compose exec hub gu-hub peer list
 
-# The result of this task should be 242611
 docker-compose exec hub gumpi -h "$HUB_ADDR" -j /examples/game-life.toml -n 4
+
+# Check the correctness of the output
+docker-compose exec hub cmp /opt/hub/game-life-output.txt /examples/correct-output.txt
